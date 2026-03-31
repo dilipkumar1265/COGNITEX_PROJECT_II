30647ad769d252f9d4a62b4e44e116b3d889470a
DILIP KUMAR R <119065291+dilipkumar1265@users.noreply.github.com>
2026-03-31 21:40:10 +0530

    Delete ffyp directory

--- ffyp/backend/agents/planner.py
+++
@@ -1,9 +0,0 @@
-# backend/agents/planner.py
-
-def planner_agent(cognitive_load):
-    if cognitive_load == "Underload":
-        return "Increase difficulty"
-    elif cognitive_load == "Optimal":
-        return "Maintain pace"
-    else:
-        return "Reduce difficulty"
